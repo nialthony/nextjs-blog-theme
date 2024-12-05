9fd0453fedbcf9c98459e12adcde8772b98e1eb2
nialthony <156112298+nialthony@users.noreply.github.com>
2024-12-05 09:44:43 +0000

    modified:   components/Header.js 	renamed:    components/nt-logo.jpg -> public/images/nt-logo.jpg

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,11 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <img
+        src="/images/nt-logo.jpg" // Pastikan path-nya benar (idealnya file berada di public folder)
+        alt="Custom Image"
+        className="block w-12 h-12 mx-auto mb-4 rounded-full"
+      />
       <p className="text-2xl text-center dark:text-white">
         <Link href="/">
           {name}
