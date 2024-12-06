b2880713b63ed036380c1fddb36750ca4cfa4e97
nialthony <156112298+nialthony@users.noreply.github.com>
2024-12-06 12:46:14 +0000

    modified:   components/CustomImage.js 	modified:   components/Header.js 	deleted:    posts/Test-bro.mdx 	new file:   posts/The-Big-Bybit-Unboxing.mdx 	new file:   public/images/TBBU.jpg 	new file:   public/images/csm.jpg 	modified:   utils/global-data.js

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ export default function Header({ name }) {
     <header className="pt-20 pb-12">
       <Link href="https://nialthony.t.me"> {/* Ganti "/custom-link" dengan URL tujuan */}
         <img
-          src="/images/nt-logo.jpg" // Path ke gambar
+          src="/images/csm.jpg" // Path ke gambar
           alt="Custom Image"
           className="block w-12 h-12 mx-auto mb-4 rounded-full cursor-pointer" // Tambahkan cursor-pointer untuk visualisasi klik
         />
